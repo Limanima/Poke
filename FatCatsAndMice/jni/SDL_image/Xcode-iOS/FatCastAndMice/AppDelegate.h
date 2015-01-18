80a0632b1774a0c5ad58c85884a2419354d256bc
Jorge Lima <jorge.raposo.lima@gmail.com>
2015-01-18 23:24:18 +0000

    Código do SDL Image

---
+++ FatCatsAndMice/jni/SDL_image/Xcode-iOS/FatCastAndMice/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FatCastAndMice
+//
+//  Created by Jorge Lima on 17/09/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
